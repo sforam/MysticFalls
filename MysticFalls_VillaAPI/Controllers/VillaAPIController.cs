07748c4029534dac7220918f7f6eb154314bafd3
sforam <foramshah0803@gmail.com>
2024-08-02 17:02:13 +0530

    Custom Logger

--- MysticFalls_VillaAPI/Controllers/VillaAPIController.cs
+++ MysticFalls_VillaAPI/Controllers/VillaAPIController.cs
@@ -1,7 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc;
 using MysticFalls_VillaAPI.Data;
+using MysticFalls_VillaAPI.Logging;
 using MysticFalls_VillaAPI.Models;
 using MysticFalls_VillaAPI.Models.Dto;
+
+
 
 namespace MysticFalls_VillaAPI.Controllers
 {
@@ -11,17 +15,134 @@ namespace MysticFalls_VillaAPI.Controllers
     [ApiController]
     public class VillaAPIController:ControllerBase
     {
+        private readonly ILogging _logger;   
+       
+        public VillaAPIController(ILogging logger)
+        {
+                    _logger = logger;
+        }
+
+
         [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
 
-        public IEnumerable<VillaDTO> GetVillas() 
+        public  ActionResult <IEnumerable<VillaDTO>> GetVillas() 
         {
-            return VillaStore.villaList;
+            _logger.Log("Getting all villas","" ); 
+            return Ok(VillaStore.villaList);
         }
-        [HttpGet("{id:int}")]
-        public VillaDTO GetVillas(int id)
+
+        [HttpGet("{id:int}",Name ="GetVilla")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+
+/*        [ProducesResponseType(404)]
+        [ProducesResponseType(400)]
+*/
+        public ActionResult<VillaDTO> GetVillas(int id)
         {
-            return VillaStore.villaList.FirstOrDefault(u=>u.Id==id);
+            if (id == 0)
+            {
+                _logger.Log("Get Villa Error With Id"+id,"error");
+                return BadRequest();      
+            }
+           var villa = VillaStore.villaList.FirstOrDefault(u => u.Id == id);
+            if (villa == null) 
+            {
+                return NotFound();
+            }
+
+
+            return Ok(villa);
 
         }
+
+        [HttpPost]
+       
+        public ActionResult<VillaDTO> CreateVilla([FromBody]VillaDTO villaDTO)
+        {
+            /* if (!ModelState.IsValid) 
+             {
+                 return BadRequest(ModelState);
+             }*/
+
+            if (VillaStore.villaList.FirstOrDefault(u => u.Name.ToLower() == villaDTO.Name.ToLower()) != null)
+            {
+                ModelState.AddModelError("CustomError","Villa already Exists!");
+                return BadRequest(ModelState);
+            }
+            if(villaDTO == null)
+            {
+                return BadRequest(villaDTO);
+            }
+            if (villaDTO.Id > 0)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+            villaDTO.Id = VillaStore.villaList.OrderByDescending(u => u.Id).FirstOrDefault().Id+1;
+            VillaStore.villaList.Add(villaDTO);
+
+
+            return CreatedAtRoute("GetVilla",new {id=villaDTO.Id},villaDTO);
+        }
+
+        [HttpDelete("{id:int}", Name = "DelelteVilla")]
+        public IActionResult DeleteVilla(int id) 
+        {
+            if(id == 0) 
+            {
+                return BadRequest();
+            }
+            var villa = VillaStore.villaList.FirstOrDefault(u => u.Id == id);
+            if(villa == null)
+            {
+                return NotFound();
+            }
+            VillaStore.villaList.Remove(villa);
+            return Ok();
+        }
+
+        [HttpPut("")]
+        public IActionResult UpdateVilla(int id, [FromBody] VillaDTO villaDTO)
+        {
+            if (villaDTO == null || id != villaDTO.Id)
+            {
+                return BadRequest();
+            }
+            var villa = VillaStore.villaList.FirstOrDefault(u => u.Id == id);
+            villa.Name = villaDTO.Name;
+            villa.Sqft = villaDTO.Sqft;
+            villa.Occupancy = villaDTO.Occupancy;
+
+            return Ok();
+        }
+
+
+        [HttpPatch("{id:int}",Name ="UpdatePartialVilla")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+
+        public IActionResult UpdatePartialVilla(int id,JsonPatchDocument<VillaDTO> patchDTO)
+        {
+            if(patchDTO == null || id == 0)
+            {
+                return BadRequest();
+            }
+            var villa = VillaStore.villaList.FirstOrDefault(u=> u.Id == id);
+            if(villa == null)
+            {
+                return BadRequest();
+            }
+
+            patchDTO.ApplyTo(villa, ModelState);
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            return NoContent();
+        }
+
+
     }
 }
---
+++ MysticFalls_VillaAPI/Logging/ILogging.cs
@@ -0,0 +1,7 @@
+﻿namespace MysticFalls_VillaAPI.Logging
+{
+    public interface ILogging
+    {
+        public void Log(string message,string type);
+    }
+}
